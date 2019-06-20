488321285263b8ad837296747cf2c6de91ab6e62
Brandon Spendlove <brandon-spendlove@hotmail.co.uk>
2019-06-20 17:37:43 +0100

    AAA Changes

--- IOSManager/bcpiosmanager/bcpIOSapi/controllers/controller_aaa.py
+++ IOSManager/bcpiosmanager/bcpIOSapi/controllers/controller_aaa.py
@@ -8,8 +8,14 @@ class AaaAPI(object):
             self.iosapi = IOSAPI()
 
     #Add Functions
+    #add_aaa_group is best used with with add_radius_server or add_tacacs_server
+    #like:  myRadiusServer = add_radius_server('10.10.10.10','mykey','1812','1813')
+    #       add_aaa_group('MyRadiusGroup', myRadiusServer)
     def add_aaa_group(self, name='', type='radius', ip=''):
         cmds = []
+
+        if not name:
+            return("AAA Group needs name to be configured...")
 
         if type == 'radius':
             cmds.append('aaa group server radius %s' %(name))
@@ -18,10 +24,25 @@ class AaaAPI(object):
 
         cmds.append('server %s' %(ip))
 
+        output = self.iosapi.bcp_send_config_command(self.iosapi.netmiko_session, cmds)
+        self.iosapi.bcp_log("info", "(%s) add_aaa_group() : Attempting to add aaa group %s (%s)" %(__name__, name, type))
+
+        return(output)
+
     def add_radius_server(self, server, key, auth_port, acct_port):
         cmd = 'radius-server host %s auth-port %s acct-port %s key %s' %(server, auth_port, acct_port, key)
         output = self.iosapi.bcp_send_config_command(self.iosapi.netmiko_session, cmd)
         self.iosapi.bcp_log("info", "(%s) add_radius_server() : Attempting to add radius server %s" %(__name__, server))
+        return(server)
+
+    def add_tacacs_server(self, server, key, port=''):
+        if port:
+            cmd = 'tacacs-server host %s port %s key %s' %(server, port, key)
+        else:
+            cmd = 'tacacs-server host %s key %s' %(server, key)
+
+        output = self.iosapi.bcp_send_config_command(self.iosapi.netmiko_session, cmd)
+        self.iosapi.bcp_log("info", "(%s) add_tacacs_server() : Attempting to add tacacs server %s" %(__name__, server))
         return(server)
 
     def add_local_user(self, username, password='', secret='', priv_level=15):
@@ -49,6 +70,35 @@ class AaaAPI(object):
         self.iosapi.bcp_log("info", "(%s) set_aaa_new_model : Attempting to send command aaa new-model" %(__name__))
         return(output)
 
+    def set_aaa_authentication_login(self, auth_list='', local=True, group=False, group_name='', local_fallback=False):
+        if auth_list:
+            if local:
+                cmd = 'aaa authentication login %s local' %(auth_list)
+            else:
+                if group:
+                    if not group_name:
+                        return("Group name needs to be configured...")
+                    else:
+                        cmd = 'aaa authentication login %s group %s' %(auth_list, group_name)
+                    if local_fallback:
+                        cmd = 'aaa authentication login %s group %s local' %(auth_list, group_name)
+        else:
+            if local:
+                cmd = 'aaa authentication login %s local' %(auth_list)
+            else:
+                if group:
+                    if not group_name:
+                        return("Group name needs to be configured...")
+                    else:
+                        cmd = 'aaa authentication login default group %s' %(group_name)
+                    if local_fallback:
+                        cmd = 'aaa authentication login default group %s local' %(group_name)
+
+        output = self.iosapi.bcp_send_command(self.iosapi.netmiko_session, cmd)
+        self.iosapi.bcp_log("info", "(%s) set_aaa_authentication_login : Attempting to send command %s" %(__name__, command))
+        return(output)
+
+
     def set_enable(self, pwd, type='secret'):
         if type == 'secret':
             cmd = 'enable secret %s' %(pwd)
@@ -56,6 +106,7 @@ class AaaAPI(object):
             cmd = 'enable password %s' %(pwd)
 
         output = self.iosapi.bcp_send_config_command(self.iosapi.netmiko_session, cmd)
+        self.iosapi.bcp_log("info", "(%s) set_enable : Attempting to set enable" %(__name__))
         return(output)
 
     #Get Functions
